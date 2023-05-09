30edbcf08f49d600e7e670b18c7404622c586ad9
Cplanchet <masterblasterplanchet@gmail.com>
2023-05-09 16:00:21 -0400

    Add repository implementation

--- app/src/main/java/me/cplanchet/shoppinglistassistant/data/ManualShoppingListRepository.kt
+++ app/src/main/java/me/cplanchet/shoppinglistassistant/data/ManualShoppingListRepository.kt
@@ -1,11 +1,21 @@
 package me.cplanchet.shoppinglistassistant.data
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import me.cplanchet.shoppinglistassistant.data.entities.Category
+import me.cplanchet.shoppinglistassistant.data.entities.Item
+import me.cplanchet.shoppinglistassistant.data.entities.ListItem
 import me.cplanchet.shoppinglistassistant.data.entities.ShoppingList
 
 class ManualShoppingListRepository : ShoppingListRepository{
     override fun GetAllLists(): Flow<List<ShoppingList>> {
-        TODO("Not yet implemented")
+        val category = Category(1, "cat 1")
+        val item = Item(1, "item1", category)
+        val listItems = listOf(
+            ListItem(item, 1f, "count", false),
+            ListItem(item, 3f, "lb", false),
+        )
+        val shoppingList = listOf(ShoppingList(1, "name", listItems, null), ShoppingList(2, "name2", listItems, null))
+        return flowOf(shoppingList)
     }
-
 }
