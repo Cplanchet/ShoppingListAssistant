a3afe2bf9965d77c4cd4a93577d0255f5ecbdf1a
Cplanchet <masterblasterplanchet@gmail.com>
2023-06-05 09:09:59 -0400

    Add tests

--- app/src/androidTest/java/me/cplanchet/shoppinglistassistant/ListCardTest.kt
+++ app/src/androidTest/java/me/cplanchet/shoppinglistassistant/ListCardTest.kt
@@ -3,8 +3,8 @@ package me.cplanchet.shoppinglistassistant
 import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onChildren
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onParent
 import me.cplanchet.shoppinglistassistant.data.entities.Category
 import me.cplanchet.shoppinglistassistant.data.entities.Item
 import me.cplanchet.shoppinglistassistant.data.entities.ListItem
@@ -30,8 +30,8 @@ class ListCardTest {
 
     val shoppingListNoItems = ShoppingList(1, "No Items", listOf<ListItem>(), null)
     val shoppingListOneItem = ShoppingList(2, "One Item", listOf(listItem1), null)
-    val shoppingListThreeItems = ShoppingList(2, "Three Item", listOf(listItem1, listItem2, listItem3), null)
-    val shoppingListFourItems = ShoppingList(2, "One Item", listOf(listItem1, listItem2, listItem3, listItem4), null)
+    val shoppingListThreeItems = ShoppingList(2, "Three Items", listOf(listItem1, listItem2, listItem3), null)
+    val shoppingListFourItems = ShoppingList(2, "Four Items", listOf(listItem1, listItem2, listItem3, listItem4), null)
 
     @get:Rule
     val composeTestRule = createComposeRule()
@@ -43,8 +43,41 @@ class ListCardTest {
                 ListCard(list = shoppingListNoItems)
             }
         }
-
         composeTestRule.onNodeWithText("No Items").assertExists()
-        composeTestRule.onNodeWithTag("Card Item List").onChildren().assertCountEquals(0)    //Go to the row that should contain no items
+        composeTestRule.onNodeWithText("No Items").onParent().onChildren().assertCountEquals(2) //Assert there is now items column
+    }
+
+    @Test
+    fun listCard_oneItem_displaysItem(){
+        composeTestRule.setContent {
+            ShoppingListAssistantTheme {
+                ListCard(list = shoppingListOneItem)
+            }
+        }
+        composeTestRule.onNodeWithText("One Item").assertExists()
+        composeTestRule.onNodeWithText("fake item 1").onParent().onChildren().assertCountEquals(4)  //should only have one item in list
+    }
+
+    @Test
+    fun listCard_threeItems_displaysItemsWithNoEllipses(){
+        composeTestRule.setContent {
+            ShoppingListAssistantTheme {
+                ListCard(list = shoppingListThreeItems)
+            }
+        }
+        composeTestRule.onNodeWithText("Three Items").assertExists()
+        composeTestRule.onNodeWithText("fake item 1").onParent().onChildren().assertCountEquals(8)  //two more list items and two more check boxes
+    }
+
+    @Test
+    fun listCard_fourItems_displaysThreeItemsWithEllipses(){
+        composeTestRule.setContent {
+            ShoppingListAssistantTheme {
+                ListCard(list = shoppingListFourItems)
+            }
+        }
+        composeTestRule.onNodeWithText("Four Items").assertExists()
+        composeTestRule.onNodeWithText("fake item 1").onParent().onChildren().assertCountEquals(9) //same as 3, but add ellipses
+        composeTestRule.onNodeWithText("...").assertExists()
     }
 }
