508f63891c5f1a40f6f776d5e17cdf2227dd5c29
Cplanchet <masterblasterplanchet@gmail.com>
2024-03-06 15:04:32 -0500

    Add Last UIState Tests

---
+++ app/src/test/java/me/cplanchet/shoppinglistassistant/uistate/ListUIStateTest.kt
@@ -0,0 +1,73 @@
+package me.cplanchet.shoppinglistassistant.uistate
+
+import me.cplanchet.shoppinglistassistant.data.dtos.ShoppingListDto
+import me.cplanchet.shoppinglistassistant.fakes.DaoMockData
+import me.cplanchet.shoppinglistassistant.ui.state.ListUIState
+import me.cplanchet.shoppinglistassistant.ui.state.isValid
+import me.cplanchet.shoppinglistassistant.ui.state.toListDto
+import me.cplanchet.shoppinglistassistant.ui.state.toListUIState
+import org.junit.Assert.*
+import org.junit.Test
+class ListUIStateTest {
+    private val listItems = DaoMockData.allListItemDtosList1
+    private val store = DaoMockData.store1Dto
+    @Test
+    fun listUIState_defaultConstructor_createsUIState() {
+        val expected = ListUIState(0, "", ArrayList(), null)
+
+        val actual = ListUIState()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun listUIState_constructor_creates_UIState() {
+        val actual = ListUIState(1, "name", listItems, store)
+
+        assertEquals(1, actual.id)
+        assertEquals("name", actual.name)
+        assertEquals(listItems, actual.items)
+        assertEquals(store, actual.store)
+    }
+
+    @Test
+    fun toListDto_givenUIState_createsDto(){
+        val expected = ShoppingListDto(1, "name", listItems, store)
+        val uiState = ListUIState(1, "name", listItems, store)
+
+        val actual = uiState.toListDto()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun toListUIState_givenDto_createsUIState(){
+        val expected = ListUIState(1, "name", listItems, store)
+        val dto = ShoppingListDto(1, "name", listItems, store)
+
+        val actual = dto.toListUIState()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun isValid_validUIState_returnsTrue(){
+        val validUIState = ListUIState(1, "name", ArrayList(), null)
+
+        assertTrue(validUIState.isValid())
+    }
+
+    @Test
+    fun isValid_missingName_returnsFalse(){
+        val invalidUIState = ListUIState(1, "", ArrayList(), null)
+
+        assertFalse(invalidUIState.isValid())
+    }
+
+    @Test
+    fun isValid_longName_returnsFalse(){
+        val invalidUIState = ListUIState(1, "loooooooooooooooooooooooongname", ArrayList(), null)
+
+        assertFalse(invalidUIState.isValid())
+    }
+}
---
+++ app/src/test/java/me/cplanchet/shoppinglistassistant/uistate/StoreUIStateTest.kt
@@ -0,0 +1,75 @@
+package me.cplanchet.shoppinglistassistant.uistate
+
+import me.cplanchet.shoppinglistassistant.data.dtos.AisleDto
+import me.cplanchet.shoppinglistassistant.data.dtos.StoreDto
+import me.cplanchet.shoppinglistassistant.fakes.DaoMockData
+import me.cplanchet.shoppinglistassistant.ui.state.StoreUIState
+import me.cplanchet.shoppinglistassistant.ui.state.isValid
+import me.cplanchet.shoppinglistassistant.ui.state.toStoreDto
+import me.cplanchet.shoppinglistassistant.ui.state.toUIState
+import org.junit.Assert
+import org.junit.Test
+
+class StoreUIStateTest {
+    private val items = DaoMockData.allItemDtos
+    private val aisle = AisleDto(1, "name", DaoMockData.allCategoryDtos, DaoMockData.allItemDtos)
+    @Test
+    fun storeUIState_defaultConstructor_createsUIState() {
+        val expected = StoreUIState(0, "", listOf(), listOf())
+
+        val actual = StoreUIState()
+
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun storeUIState_constructor_creates_UIState() {
+        val actual = StoreUIState(1, "name", items, listOf(aisle))
+
+        Assert.assertEquals(1, actual.id)
+        Assert.assertEquals("name", actual.name)
+        Assert.assertEquals(items, actual.items)
+        Assert.assertEquals(listOf(aisle), actual.aisles)
+    }
+
+    @Test
+    fun toStoreDto_givenUIState_createsDto(){
+        val expected = StoreDto(1, "name", items, listOf(aisle))
+        val uiState = StoreUIState(1, "name", items, listOf(aisle))
+
+        val actual = uiState.toStoreDto()
+
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun toStoreUIState_givenDto_createsUIState(){
+        val expected = StoreUIState(1, "name", items, listOf(aisle))
+        val dto = StoreDto(1, "name", items, listOf(aisle))
+
+        val actual = dto.toUIState()
+
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun isValid_validUIState_returnsTrue(){
+        val validUIState = StoreUIState(1, "name", listOf(), listOf())
+
+        Assert.assertTrue(validUIState.isValid())
+    }
+
+    @Test
+    fun isValid_missingName_returnsFalse(){
+        val invalidUIState = StoreUIState(1, "", listOf(), listOf())
+
+        Assert.assertFalse(invalidUIState.isValid())
+    }
+
+    @Test
+    fun isValid_longName_returnsFalse(){
+        val invalidUIState = StoreUIState(1, "loooooooooooooooooooooooongname", listOf(), listOf())
+
+        Assert.assertFalse(invalidUIState.isValid())
+    }
+}
