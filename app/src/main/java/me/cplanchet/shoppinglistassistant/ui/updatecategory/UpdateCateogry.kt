34ed3f639014a32a35121034e615d19b0e44a74e
Cplanchet <masterblasterplanchet@gmail.com>
2024-03-15 22:33:55 -0400

    Update UI

--- app/src/main/java/me/cplanchet/shoppinglistassistant/ui/updatecategory/UpdateCateogry.kt
+++ app/src/main/java/me/cplanchet/shoppinglistassistant/ui/updatecategory/UpdateCateogry.kt
@@ -1,6 +1,7 @@
 package me.cplanchet.shoppinglistassistant.ui.updatecategory
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -8,6 +9,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.cplanchet.shoppinglistassistant.R
 import me.cplanchet.shoppinglistassistant.ui.components.AppBar
 import me.cplanchet.shoppinglistassistant.ui.theme.ShoppingListAssistantTheme
@@ -21,8 +24,9 @@ fun UpdateCategoryPage(
     var temp by remember{mutableStateOf("")}
     Scaffold(topBar = { AppBar(hasBackButton = true, navigateUp = onNavigateUp) }){ paddingValues ->
         Column(
-            modifier = modifier.padding(paddingValues),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = modifier.padding(paddingValues).then(Modifier.padding(all = 32.dp)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             Text(
                 text = stringResource(R.string.update_category_title),
@@ -34,30 +38,32 @@ fun UpdateCategoryPage(
                 modifier = Modifier.fillMaxWidth(),
                 value = temp,
                 onValueChange = { temp = it },
-                label = { Text( text = stringResource(R.string.create_category_label_name)) }
+                label = { Text( text = stringResource(R.string.create_category_label_name)) },
             )
             Button(
                 onClick = {},
                 colors = ButtonDefaults.buttonColors(
                     contentColor = MaterialTheme.colorScheme.onError,
                     containerColor = MaterialTheme.colorScheme.error),
+                modifier = Modifier.widthIn(min = 100.dp)
                 ){
                 Text(text = stringResource(R.string.delete))
             }
             Row(
-                modifier = Modifier,
+                modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ){
                 OutlinedButton(
                     onClick = {},
-                    modifier = Modifier
+                    modifier = Modifier.widthIn(min = 100.dp),
+                    border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
                 ){
                     Text(stringResource(R.string.cancel))
                 }
                 Button(
                     onClick = {},
-                    modifier = Modifier
+                    modifier = Modifier.widthIn(min = 100.dp)
                 ){
                     Text(stringResource(R.string.save))
                 }
